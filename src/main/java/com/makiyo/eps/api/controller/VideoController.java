44b1541d10652c2ae3d5cfa2b85e1be67e0962d2
Makiyo <lin171820@gmail.com>
2023-02-16 12:17:03 +0800

    监控开发

---
+++ src/main/java/com/makiyo/eps/api/controller/VideoController.java
@@ -0,0 +1,29 @@
+package com.makiyo.eps.api.controller;
+
+import com.makiyo.eps.api.service.VideoService;
+import com.makiyo.eps.api.utils.Response;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/video")
+@Tag(name = "VideoController", description = "用户Web接口")
+public class VideoController {
+    @Autowired
+    private VideoService videoService;
+
+    @PostMapping("/fatigueDetection")
+    public Response fatigueDetection(@RequestBody Map<String,String> map) {
+        System.out.println(map.get("photo"));
+        String image = videoService.fatigueDetection(map.get("photo"));
+        Response response = new Response();
+        response.put("image",image);
+        return response;
+    }
+}
